5b90d88b158016474fb696a373df5c3834a00110
Séverine Dorimond Pereira <doris190485@hotmail.com>
2024-06-19 23:27:43 +0200

    map created with provinces visible on map

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import building from '../services/buildings';
+
 
 function Map() {
-  
 
+  const buildings = building.batiments;
 
-  return (
-    <canvas>
+  let canvasRef = useRef(null);
+
+  useEffect(() => {
+    function drawBuilding(building) {
+      if (building.state === 'enable') {
+        const canvas = canvasRef.current;
+        
+        canvas.width = window.innerWidth; 
+        canvas.height = window.innerHeight; 
+
+        const ctx = canvas.getContext('2d');
+        const image = new Image();
+        console.log(image, 'image');
+
       
-    </canvas>
-  );
+
+        image.src = building.image;
+        console.log(image.src, 'image');
+        image.onload = () => {
+         
+          ctx.drawImage(
+            image,
+            building.positionX,
+            building.positionY,
+            building.width,
+            building.height
+          );
+        };
+      }
+    }
+
+    buildings.map(building => {
+      drawBuilding(building)
+    });
+
+  }, [buildings]);
+
+  return <canvas ref={canvasRef}></canvas>;
 }
 
-export default Map
+export default Map;
---
+++ frontend/src/services/buildings.js
@@ -0,0 +1,151 @@
+import image1 from '../assets/province1.png';
+import image2 from '../assets/province2.png';
+import image3 from '../assets/province3.png';
+import image4 from '../assets/province4.png';
+import image5 from '../assets/province5.png';
+import image6 from '../assets/province6.png';
+import image7 from '../assets/province7.png';
+import image8 from '../assets/province8.png';
+import image9 from '../assets/province9.png';
+import image10 from '../assets/province10.png';
+import image11 from '../assets/province11.png';
+import image12 from '../assets/province12.png';
+import image13 from '../assets/province13.png';
+import image14 from '../assets/province14.png';
+
+
+
+
+
+const building = {
+  batiments: [
+    {
+      name: 'Batiment 1',
+      positionX: 150,
+      positionY: 150,
+      state: 'enable',
+      image: `${image1}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 2',
+      positionX: 200,
+      positionY: 50,
+      state: 'enable',
+      image: `${image2}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 3',
+      positionX: 20,
+      positionY: 150,
+      state: 'enable',
+      image: `${image3}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 4',
+      positionX: 30,
+      positionY: 50,
+      state: 'enable',
+      image: `${image4}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 5',
+      positionX: 30,
+      positionY: 250,
+      state: 'enable',
+      image: `${image5}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 6',
+      positionX: 200,
+      positionY: 350,
+      state: 'enable',
+      image: `${image6}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 7',
+      positionX: 150,
+      positionY: 250,
+      state: 'enable',
+      image: `${image7}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 8',
+      positionX: 100,
+      positionY: 400,
+      state: 'enable',
+      image: `${image8}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 9',
+      positionX: 90,
+      positionY: 310,
+      state: 'enable',
+      image: `${image9}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 10',
+      positionX: 200,
+      positionY: 450,
+      state: 'enable',
+      image: `${image10}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 11',
+      positionX: 250,
+      positionY: 150,
+      state: 'enable',
+      image: `${image11}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 12',
+      positionX: 100,
+      positionY: 90,
+      state: 'enable',
+      image: `${image12}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 13',
+      positionX: 150,
+      positionY: 150,
+      state: 'enable',
+      image: `${image13}`,
+      width: '150',
+      height: '150',
+    },
+    {
+      name: 'Batiment 14',
+      positionX: 30,
+      positionY: 450,
+      state: 'enable',
+      image: `${image14}`,
+      width: '150',
+      height: '150',
+    },
+  ],
+};
+
+export default building;
