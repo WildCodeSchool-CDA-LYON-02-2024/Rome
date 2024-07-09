bb50b44c34943f47599411a869e4d93fd1df85f0
Patrick Hoang <hoang.patrick@outlook.com>
2024-07-09 15:50:35 +0200

    page intro ok

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./Home.css";
 import ceasar from "/videos/ceasar.mp4";
@@ -6,18 +7,49 @@ import ceasar2 from "/videos/ceasar2.mp4";
 import ceasar3 from "/videos/ceasar3.mp4";
 import ceasar4 from "/videos/ceasar4.mp4";
 import ceasar5 from "/videos/ceasar5.mp4";
+import intro from "/sound/music.mp3";
+import button2 from "/sound/button2.mp3";
 
 function Home() {
   const videos = [ceasar, ceasar2, ceasar3, ceasar4, ceasar5];
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
   const videoRef = useRef(null);
+  const audioRef = useRef(null);
+  const audioRef1 = useRef(null);
+  const [redirect, setRedirect] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (redirect) {
+      const timer = setTimeout(() => {
+        navigate("/user/login");
+      }, 500); // 0.5 seconds delay
+
+      return () => clearTimeout(timer);
+    }
+  }, [redirect, navigate]);
 
   const handleEnded = () => {
     setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videos.length);
   };
 
+  const handlePlaySound1 = () => {
+    if (audioRef1.current) {
+      audioRef1.current.play();
+    }
+    setRedirect(true);
+  };
+
   return (
     <div className="background-video-container">
+            <audio ref={audioRef} src={intro} />
+            <audio ref={audioRef1} src={button2} />
       <video
         ref={videoRef}
         autoPlay
@@ -31,9 +63,9 @@ function Home() {
       </video>
       <div className="content">
         <h1>Rome Antique</h1>
-        <Link to={"/user/login"}>
-          <button className="homeButton">Commencez votre aventure </button>
-        </Link>
+        <button onClick={handlePlaySound1} className="homeButton">
+          Commencez votre aventure
+        </button>
       </div>
     </div>
   );
